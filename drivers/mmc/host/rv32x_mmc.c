be441802456491add643a4537a6741d38f68cc1c
twinkletia <vamosidemochumjetze215@gmail.com>
2023-02-01 10:42:21 +0900

    minor fix

--- drivers/mmc/host/rv32x_mmc.c
+++ drivers/mmc/host/rv32x_mmc.c
@@ -22,7 +22,6 @@
 #define RV32X_MMC_RESP		0x208
 #define RV32X_MMC_CMD		0x20C
 #define RV32X_MMC_ARG		0x210
-#define RV32X_MMC_CSD_CID_BASE	0x214
 #define INITED 0x1
 #define IDLE 0x2
 #define EXEC 0x4
@@ -34,48 +33,45 @@ struct rv32x_mmc {
 	void __iomem      *regbase;
 	void* kmap_addr;
 	unsigned blk_cnt;
+	unsigned transfer_len;
 };
 
 
 static inline struct mmc_command* data_to_cmd(struct mmc_data**);
-static void rv32x_mmc_read_block(struct rv32x_mmc*, struct mmc_data*);
-static void rv32x_mmc_write_block(struct rv32x_mmc*, struct mmc_data*);
-static int rv32x_mmc_transfer_data(struct rv32x_mmc*, struct mmc_data *);
+static void rv32x_mmc_read_block(struct rv32x_mmc*, struct mmc_command*);
+static void rv32x_mmc_write_block(struct rv32x_mmc*, struct mmc_command*);
+static int rv32x_mmc_transfer_data(struct rv32x_mmc*, struct mmc_command *);
 static int rv32x_mmc_do_command(struct rv32x_mmc*, u32, u32, u32*);
 
 static inline struct mmc_command* data_to_cmd(struct mmc_data** data_ptr){
 	return container_of(data_ptr, struct mmc_command, data);
 }
 
-static void rv32x_mmc_read_block(struct rv32x_mmc* rv32x, struct mmc_data* data){
-	unsigned* data_ptr;
+static void rv32x_mmc_read_block(struct rv32x_mmc* rv32x, struct mmc_command *cmd){
 	unsigned i = 0;
-	struct mmc_command* cmd = data_to_cmd(&data);
-	rv32x_mmc_do_command(rv32x, 17, cmd->arg + (rv32x->blk_cnt*512), cmd->resp);	//argはdata->blk_addrでいいかも
+	unsigned* memaddr = rv32x->kmap_addr;
+	rv32x_mmc_do_command(rv32x, cmd->opcode != 18 ? cmd->opcode : 17, cmd->arg + (rv32x->blk_cnt*512), cmd->resp);	//argはdata->blk_addrでいいかも
 	rv32x->blk_cnt++;
-	data_ptr = rv32x->kmap_addr;
-	while(i < RV32X_MMC_STATUS){
-		*data_ptr++ = ioread32(rv32x->regbase+RV32X_MMC_DATA_BASE+i);
+	while(i < rv32x->transfer_len){
+		*memaddr++ = ioread32(rv32x->regbase+RV32X_MMC_DATA_BASE+i);
 		i+=4;
 	}
 }
 
-static void rv32x_mmc_write_block(struct rv32x_mmc* rv32x, struct mmc_data* data){
-	unsigned* data_ptr;
+static void rv32x_mmc_write_block(struct rv32x_mmc* rv32x, struct mmc_command *cmd){
 	unsigned i = 0;
-	struct mmc_command* cmd = data_to_cmd(&data);
-	data_ptr = rv32x->kmap_addr;
-	while(i < RV32X_MMC_STATUS){
-		iowrite32(*data_ptr++,rv32x->regbase+RV32X_MMC_DATA_BASE+i);
+	unsigned* memaddr = rv32x->kmap_addr;
+	while(i < rv32x->transfer_len){
+		iowrite32(*memaddr++,rv32x->regbase+RV32X_MMC_DATA_BASE+i);
 		i+=4;
 	}
-	rv32x_mmc_do_command(rv32x, 24, cmd->arg + (rv32x->blk_cnt*512), cmd->resp);	//argはdata->blk_addrでいいかも
+	rv32x_mmc_do_command(rv32x, cmd->opcode != 25 ? cmd->opcode : 24, cmd->arg + (rv32x->blk_cnt*512), cmd->resp);	//argはdata->blk_addrでいいかも
 	rv32x->blk_cnt++;
 }
 
-static int rv32x_mmc_transfer_data(struct rv32x_mmc* rv32x,
-	struct mmc_data *data)
+static int rv32x_mmc_transfer_data(struct rv32x_mmc* rv32x, struct mmc_command *cmd)
 {
+	struct mmc_data* data = cmd->data;
 	struct scatterlist	*sg;
 	unsigned		i;
 	int			multiple = (data->blocks > 1);
@@ -87,27 +83,31 @@ static int rv32x_mmc_transfer_data(struct rv32x_mmc* rv32x,
 	for_each_sg(data->sg, sg, data->sg_len, i){
 		length = sg->length;
 		size = 0;
-
+		/*
+		pr_debug("sg length:%d",length);
+		pr_debug("sg offset:%08x",sg->offset);
+		*/
 		/* allow pio too; we don't allow highmem */
-		rv32x->kmap_addr = kmap(sg_page(sg)) + sg->offset;
+		rv32x->kmap_addr = sg_virt(sg);
 
 		/* transfer each block, and update request status */
 		while (length) {
 			rv32x->kmap_addr += size;
 			size = min(length, data->blksz);
+			rv32x->transfer_len = size;
+			//pr_debug("kmap addr:%x",rv32x->kmap_addr);
+			//pr_debug("size:%d",size);
+			
 			if(data->flags & MMC_DATA_READ){
-				rv32x_mmc_read_block(rv32x, data);
+				rv32x_mmc_read_block(rv32x, cmd);
 			}else{
-				rv32x_mmc_write_block(rv32x, data);
+				rv32x_mmc_write_block(rv32x, cmd);
 			}
 			data->bytes_xfered += size;
 			length -= size;
 			if (!multiple)
 				break;
 		}
-	}
-	for_each_sg(data->sg, sg, data->sg_len, i){
-		kunmap(sg_page(sg));
 	}
 
 	return 0;
@@ -140,14 +140,23 @@ static int rv32x_mmc_do_command(struct rv32x_mmc* rv32x, u32 command, u32 arg, u
 
 static void rv32x_mmc_request(struct mmc_host *mmc, struct mmc_request *mrq)
 {
+
 	mrq->cap_cmd_during_tfr = true;	//no irq wait
 
 	struct mmc_command *cmd = mrq->cmd;
-	struct mmc_data *data = mrq->cmd->data;
 	struct rv32x_mmc* rv32x = mmc_priv(mmc);
 
-	if(data){
-		rv32x_mmc_transfer_data(rv32x, data);
+	/*
+	pr_debug("cmd ptr:%x",mrq->cmd);
+	pr_debug("cmd addr:%x",&(mrq->cmd));
+	pr_debug("data ptr:%x",cmd->data);
+	pr_debug("data addr:%x",&(cmd->data));
+	pr_debug("container cmd ptr:%x",data_to_cmd(&(cmd->data)));
+	*/
+	if(cmd->opcode == 0 || cmd->opcode == 1 || cmd->opcode == 8 || cmd->opcode == 41){
+		goto done;
+	}else if(cmd->data){
+		rv32x_mmc_transfer_data(rv32x, cmd);
 	}else if(mrq->sbc){
 		mrq->sbc->resp[0] = 0;
 	}else if(mrq->stop){
@@ -155,14 +164,8 @@ static void rv32x_mmc_request(struct mmc_host *mmc, struct mmc_request *mrq)
 	}else{
 		rv32x_mmc_do_command(rv32x, cmd->opcode, cmd->arg, cmd->resp);
 	}
-	/*
-	pr_debug("mmc_request mrq->cmd:%x",mrq->cmd);
-	pr_debug("mmc_request mrq->cmd->opcode:%x",mrq->cmd->opcode);
-	pr_debug("mmc_request mrq->cmd->arg:%x",mrq->cmd->arg);
-	pr_debug("mmc_request mrq->sbc:%x",mrq->sbc);
-	pr_debug("mmc_request mrq->stop:%x",mrq->stop);
-	pr_debug("mmc_request mrq->data:%x",mrq->data);
-	*/
+done:
+	mmc_request_done(mmc, mrq);
 }
 
 static void rv32x_mmc_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
@@ -201,7 +204,7 @@ static int rv32x_mmc_init(struct platform_device *pdev)
 	mmc->ops = &rv32x_mmc_host;
 	mmc->ocr_avail = MMC_VDD_32_33|MMC_VDD_33_34;
 	mmc->caps = MMC_CAP_SPI;
-	mmc->caps2 = MMC_CAP2_NO_SDIO | MMC_CAP2_NO_SD | MMC_CAP2_NO_WRITE_PROTECT;
+	mmc->caps2 = MMC_CAP2_NO_SDIO | MMC_CAP2_NO_MMC | MMC_CAP2_NO_WRITE_PROTECT;
 	rv32x = mmc_priv(mmc);
 	rv32x->regbase = devm_platform_ioremap_resource(pdev, 0);
 	//pr_debug("rv32x->regbase:%x",rv32x->regbase);
